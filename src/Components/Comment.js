896df0b87cf1faabee826286960f9e74e509eef5
LucasButto <lbutto40@gmail.com>
2023-02-05 22:22:50 -0300

    comments changed

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -6,7 +6,7 @@ import "../Styles/Comment.css";
 const Comment = () => {
   const { currentSessionType } = useTimer();
   const shownComment =
-    currentSessionType === "Session" ? "Time to Work" : "Take a Rest";
+    currentSessionType === "Session" ? "Time to work" : "Take a break";
   return (
     <>
       <p className="comment">{shownComment}</p>
